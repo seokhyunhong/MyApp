89c5146d07976da8e4d1a60f18ee4cb5020adbcc
seokhyunhong <66937402+seokhyunhong@users.noreply.github.com>
2020-06-16 16:31:06 +0900

    Add files via upload

---
+++ JavaWork/2020_05_22/ExamTest.java
@@ -0,0 +1,13 @@
+public class ExamTest
+{
+	public static void main(String[] args)
+	{
+		int a = 10;	
+		System.out.print("a = ");
+		System.out.println(a);
+
+		String name = "Æë¼ö";
+		System.out.print("name = ");
+		System.out.print(name);
+	}
+}
